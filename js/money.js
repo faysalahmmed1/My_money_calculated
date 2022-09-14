2c6324a3a6858c55e19dc13fb84aaa7a3ea3f5f9
fkfisu <faysalahmmedcse@gmail.com>
2022-09-15 02:32:12 +0600

    my secand commit

--- js/money.js
+++ js/money.js
@@ -1,15 +1,17 @@
 document.getElementById('Total-cost-button').addEventListener('click', function () {
     const foodCost = document.getElementById('food-cost');
     const foodCostAmount = foodCost.value;
-    console.log(foodCostAmount);
-
-    //clear input field 
-    foodCost.value = '';
 
     // get current cost 
     const TotalCost = document.getElementById('total-expenses');
     const totalCostNumber = TotalCost.innerText;
+    TotalCost.innerText = foodCostAmount;
     console.log(totalCostNumber);
+
+    //clear input field 
+    foodCost.value = '';
+
+
 
 
 })
